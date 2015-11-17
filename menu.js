83a604fd42751d213acc881a8f1249cd946e19fb
leahrwolf <leahrwolf@gmail.com>
2015-11-17 08:35:40 -0500

    Created menu with JS

---
+++ menu.js
@@ -0,0 +1,13 @@
+function createMenu (menu) {
+	// this is a comment
+	document.getElementById(menu).innerHTML = "<ul>\
+	<li><a href='home.html' target='_self'>Home</a></li>\
+	<li><a href='pythonHome.html' target='_self'>Python</a></li>\
+	<li><a href='gameTheoryHome.html' target='_self'>Game Theory</a></li>\
+	<li><a href='EncryptionHome.html' target='_self'>Encryption/Decryption</a></li>\
+	<li>Web Development</li>\
+	<li>Artificial Intelligence</li>\
+	<li>About the Authors</li>\
+</ul>";
+}
+
