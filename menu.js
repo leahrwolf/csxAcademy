3e6d7998d1de32ef1f5ab552ebdb5dfb8a1a30e2
NotTommy <nathan.battles@worcesteracademy.org>
2015-12-03 10:11:11 -0500

    Fixed

---
+++ menu.js
@@ -0,0 +1,10 @@
+function createMenu () {
+	document.getElementById("homeMenu").innerHTML = "";
+
+}
+
+function getInfo () {
+	
+	var randomInfo = document.getElementById("title").value;
+	alert(randomInfo)
+}
