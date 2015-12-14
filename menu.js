7ced7b327a46f67b6aaebbd4d70f680d6f49b44b
NotTommy <nathan.battles@worcesteracademy.org>
2015-12-14 08:26:59 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	pythonHome.html
    #	pythonStartingWith.html
    #	webdevhtml.html

--- menu.js
+++ menu.js
@@ -1,8 +1,14 @@
 function createMenu (menu) {
 	// this is a comment
+<<<<<<< HEAD
 	document.getElementById(menu).innerHTML = "<button class='btn btn-primary type='button' onclick=openWindow('home.html')> Home</button>\
 	<div class='dropdown'>\
     <button class='btn btn-primary dropdown-toggle' type='button' data-toggle='dropdown'>Python\
+=======
+	document.getElementById(menu).innerHTML = "<button class='btn btn-primary type='button' onclick=openWindow('home.html') style='width:80%;'> Home</button>\
+	<div class='dropdown'>\
+    <button class='btn btn-primary dropdown-toggle' type='button' data-toggle='dropdown' style='width:80%;'>Python\
+>>>>>>> origin/master
     <span class='caret'></span></button>\
     <ul class='dropdown-menu'>\
       <li><a href='pythonHome.html' target='_self'>Python Home</a></li>\
@@ -10,7 +16,11 @@ function createMenu (menu) {
     </ul>\
   </div>\
   <div class='dropdown'>\
+<<<<<<< HEAD
     <button class='btn btn-primary dropdown-toggle' type='button' data-toggle='dropdown'>Encryption\
+=======
+    <button class='btn btn-primary dropdown-toggle' type='button' data-toggle='dropdown' style='width:80%;'>Encryption\
+>>>>>>> origin/master
     <span class='caret'></span></button>\
     <ul class='dropdown-menu'>\
       <li><a href='EncryptionHome.html' target='_self'>Encryption Home</a></li>\
@@ -19,7 +29,11 @@ function createMenu (menu) {
     </ul>\
   </div>\
   <div class='dropdown'>\
+<<<<<<< HEAD
     <button class='btn btn-primary dropdown-toggle' type='button' data-toggle='dropdown'>Game Theory\
+=======
+    <button class='btn btn-primary dropdown-toggle' type='button' data-toggle='dropdown' style='width:80%;'>Game Theory\
+>>>>>>> origin/master
     <span class='caret'></span></button>\
     <ul class='dropdown-menu'>\
       <li><a href='gameTheoryHome.html' target='_self'>Game Theory Home</a></li>\
@@ -27,6 +41,7 @@ function createMenu (menu) {
       <li><a href='nashEquilibrium.html' target='_self'>Nash Equilibrium</a></li>\
       <li><a href='p=NP.html' target='_self'>P&#61;NP</a></li>\
       <li><a href='prisonersDilemma.html' target='_self'>Prisoners Dilemma</a></li>\
+<<<<<<< HEAD
       <li><a href='rockPaperScissors.html' target='_self'>Rock Paper Scissors</a></li>\
     </ul>\
   </div>\
@@ -38,6 +53,20 @@ function createMenu (menu) {
     </ul>\
   </div>\
   <button class='btn btn-primary type='button' onclick=openWindow('abouttheauthors.html')> About the Authors</button>";
+=======
+    </ul>\
+  </div>\
+  <div class='dropdown'>\
+    <button class='btn btn-primary dropdown-toggle' type='button' data-toggle='dropdown' style='width:80%;'>Web Development\
+    <span class='caret'></span></button>\
+    <ul class='dropdown-menu'>\
+      <li><a href='webDevelopment.html' target='_self'>Web Development Home</a></li>\
+      <li><a href='javaScriptPage.html' target='_self'>Java Script</a></li>\
+      <li><a href='webdevhtml.html' target='_self'>HTML</a></li>\
+    </ul>\
+  </div>\
+  <button class='btn btn-primary type='button' onclick=openWindow('abouttheauthors.html') style='width:80%;'> About the Authors</button>";
+>>>>>>> origin/master
 }
 
 function openWindow(url){
---
+++ rps.js
@@ -0,0 +1,2 @@
+function createWorksheet (worksheet){
+	document.getElementById().worksheet= <iframe src="https://docs.google.com/document/d/1j3o3Y6cy6ypOYUV29MlS_lhgNGTRSkpOvNJ5BnAXQts/pub?embedded=true"></iframe>}
